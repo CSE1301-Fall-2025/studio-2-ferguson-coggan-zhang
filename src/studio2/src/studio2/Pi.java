2612afde66ba6d2693751e109306649964ac89de
Will Zhang <hello@willzhang.info>
2025-09-10 15:34:44 -0500

    Finish pi part

--- src/studio2/src/studio2/Pi.java
+++ src/studio2/src/studio2/Pi.java
@@ -1,7 +1,28 @@
 package studio2;
 
+import java.util.Random;
+
 public class Pi {
     public static void main(String[] args) {
-
+        Random rand = new Random();
+        double hits = 0;
+        for (int i = 0; i < 10000; i++) {
+            if (throwDart(rand)) {
+                hits++;
+            }
+        }
+        double pi = hits * 4 / 10000;
+        System.out.println(pi);
+    }
+    public static boolean throwDart(Random rand) {
+        int dartx = rand.nextInt(-1,1);
+        int darty = rand.nextInt(-1,1);
+        if (Math.sqrt(darty*darty + dartx*dartx) <= 1) {
+            System.out.println("Hit");
+            return true;
+        } else {
+            System.out.println("Miss");
+        }
+        return false;
     }
 }
